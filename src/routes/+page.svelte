72cb380fcb6c0de67330ec8a036272500f4faeb9
jebkor <j.korsbakke+github@gmail.com>
2024-07-05 01:17:45 +0200

    Added new routes and added more prompts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-  
 </script>
 
 <!-- <svelte:head>
@@ -7,10 +6,23 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head> -->
 
+
 <section class="grid grid-cols-12 px-8 justify-items-center items-center h-screen">
-  <h1>This is the frontpage</h1>
-  <p>Go to <code>/fluff</code> for some fluff and slice-of-life prompts.</p>
-  <p>Go to <code>/dates</code> for some date prompts.</p>
+	<div class="col-span-12">
+		<h1>This is the frontpage</h1>
+	</div>
+	<div class="col-span-3">
+		<p>Go to <a href="/fluff" title="/fluff">/fluff</a> for some fluff and slice-of-life prompts.</p>
+	</div>
+	<div class="col-span-3">
+    <p>Go to <a href="/dates" title="/dates">/dates</a> for some date prompts.</p>
+  </div>
+	<div class="col-span-3">
+    <p>Go to  <a href="/kiss" title="/kiss">/kiss</a> for some kissing prompts.</p>
+  </div>
+	<div class="col-span-3">
+    <p>Go to <a href="/nsfw" title="/nsfw">/nsfw</a> for some nsfw prompts.</p>
+  </div>
 </section>
 
 <style lang="scss"></style>
